cba3f057e3edf2f91e5c33d3585f5c8a4e48c945
Anand21K <andyshpandey@gmail.com>
2023-07-29 22:57:17 +0530

    completed

---
+++ deforestation_model_training.py
@@ -0,0 +1,45 @@
+
+
+! pip install -Uq fastai==2.2.5
+
+
+
+from fastai.vision.all import *
+path = Path('./planet/planet')
+
+path.ls()
+
+train_df = pd.read_csv(path/'train_classes.csv')
+train_df
+
+def get_data(size=224,bs=64,data_df=train_df):
+    dblock = DataBlock(blocks=(ImageBlock, MultiCategoryBlock),
+                       splitter=RandomSplitter(seed=42),
+                       get_x=ColReader(0, pref=f'{path}/train-jpg/', suff='.jpg'),
+                       get_y=ColReader(1, label_delim=' '),
+                       item_tfms = Resize(size),
+                       batch_tfms = [*aug_transforms(flip_vert=True, max_lighting=0.1, max_zoom=1.05, max_warp=0.),
+                                     Normalize.from_stats(*imagenet_stats)]
+                      )
+    return dblock.dataloaders(data_df,bs=bs)
+
+dls = get_data(128,256)
+
+dls.show_batch()
+
+metrics = [partial(accuracy_multi, thresh=0.2), FBetaMulti(beta=2, average='samples', thresh=0.2)]
+cbs = [MixUp]
+
+learn = cnn_learner(dls, resnet50, metrics=metrics, cbs=cbs).to_fp16()
+learn.lr_find()
+
+learn.fine_tune(6, base_lr=2e-2, freeze_epochs=4)
+
+learn.save('resnet50-128')
+
+
+
+learn.show_results()
+
+learn.export()
+
